92d89c29b5cf49159a2c597c59b581753653eb98
Romserg <serg.romankov@gmail.com>
2021-03-25 23:53:51 +0200

    Add state/reducer for user login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,20 +3,34 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
+import { Store } from '@ngrx/store';
 
 @Component({
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   pageTitle = 'Log In';
 
   maskUserName: boolean;
+  maskUserPassword: boolean;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private store: Store<any>,
+    private authService: AuthService,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit(): void {
-
+    this.store.select('users').subscribe(
+      users => {
+        if (users) {
+          this.maskUserName = users.maskUserName;
+          this.maskUserPassword = users.maskUserPassword;
+        }
+      },
+    );
   }
 
   cancel(): void {
@@ -24,7 +38,9 @@ export class LoginComponent implements OnInit {
   }
 
   checkChanged(): void {
-    this.maskUserName = !this.maskUserName;
+    this.store.dispatch(
+      { type: '[User] Toggle Mask User Name' },
+    );
   }
 
   login(loginForm: NgForm): void {
