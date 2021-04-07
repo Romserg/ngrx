de3473d9f1140bea27808ef559f9c256d491ec3f
Romserg <serg.romankov@gmail.com>
2021-04-07 22:45:38 +0300

    Divide product list component into two separate files like a container with logic and presentation

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../product';
+import { Store } from '@ngrx/store';
+import { getCurrentProduct, getError, getProducts, getShowProductCode, State } from '../state/product.reducer';
+import * as ProductActions from '../state/product.actions';
 
 @Component({
   templateUrl: './product-shell.component.html'
 })
 export class ProductShellComponent implements OnInit {
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
+  displayCode$: Observable<boolean>;
+  errorMessage$: Observable<string>;
 
-  constructor() { }
+  constructor(private store: Store<State>) {
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.store.dispatch(ProductActions.loadProducts());
+    this.products$ = this.store.select(getProducts);
+    this.errorMessage$ = this.store.select(getError);
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
+    this.displayCode$ = this.store.select(getShowProductCode);
+  }
+
+  checkChanged(): void {
+    this.store.dispatch(ProductActions.toggleProductCode());
+  }
+
+  newProduct(): void {
+    this.store.dispatch(ProductActions.initializeCurrentProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
   }
 }
